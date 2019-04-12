3b2e47e827682dc69a1fba22f5c6b534fb0bba4d
Tomawari <34960309+OtaniToma@users.noreply.github.com>
2019-04-12 11:41:30 -0700

    Create usefulMethods.js

---
+++ usefulMethods.js
@@ -0,0 +1,27 @@
+// 012 - every
+var users = [
+  { id: 21, hasSubmitted: true },
+  { id: 62, hasSubmitted: false },
+  { id: 4, hasSubmitted: true }
+];
+
+var hasSubmitted = users.every(function(user) {
+    return user.hasSubmitted === true;
+});
+
+hasSubmitted;
+
+
+
+// 013 - some
+var requests = [
+  { url: '/photos', status: 'complete' },
+  { url: '/albums', status: 'pending' },
+  { url: '/users', status: 'failed' }
+];
+
+var inProgress = requests.some(function(request) {
+  return request.status === 'pending';
+});
+
+inProgress;
